4445955c912b1797e68e570c921ce78dd3ac460e
larpPixel <199824619+larpPixel@users.noreply.github.com>
2026-05-19 12:07:36 +0530

    feat(cef-sys): iterate on shim

--- cef-sys/shim/shim.h
+++ cef-sys/shim/shim.h
@@ -10,3 +10,13 @@
 extern "C" {
 #endif
 
+// ---- event kinds delivered to the single Rust sink ------------------------
+enum {
+  CEF_EV_FRAME = 1,       // view frame updated: see rects; nrects==0 => full
+  CEF_EV_POPUP_FRAME = 2, // popup (select dropdown etc.) buffer updated
+  CEF_EV_TITLE = 3,
+  CEF_EV_URL = 4,
+  CEF_EV_LOADING = 5,
+  CEF_EV_CLOSED = 6,
+};
+
