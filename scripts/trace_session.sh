149a1681ec84e67a018903df902571747adaae3b
larpPixel <199824619+larpPixel@users.noreply.github.com>
2026-05-22 15:45:29 +0530

    chore(scripts): iterate on trace_session

--- scripts/trace_session.sh
+++ scripts/trace_session.sh
@@ -10,3 +10,10 @@ TRACE="${1:-/tmp/strip-trace.jsonl}"
 RUN="${2:-standard}"
 SOCK="${STRIP_BROWSER_SOCK:-${XDG_RUNTIME_DIR:+$XDG_RUNTIME_DIR/}strip-browser.sock}"
 SOCK="${SOCK:-/tmp/strip-browser.sock}"
+LOG="${TRACE}.log"
+
+rm -f "$TRACE" "$TRACE".log "$SOCK"
+
+STRIP_TRACE="$TRACE" RUST_LOG=warn "$ROOT/target/debug/browser" >"$LOG" 2>&1 &
+BROWSER_PID=$!
+
